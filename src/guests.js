951e4960df3b9e4ba634bde833eafe3f0a3e00e3
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-03-06 10:26:24 -0600

    Renamed func to match details in plan doc

--- src/guests.js
+++ src/guests.js
@@ -2,7 +2,7 @@ import { getGuests } from "./database.js";
 
 const guests = getGuests();
 
-export const employeesList = () => {
+export const generateGuests = () => {
     let html = "<ul>"
 
     for (const guest of guests) {
