a19c40061b7992bf02b3b510bbaf422dfec026da
Austin <cookaustin46@gmail.com>
2026-03-06 10:49:55 -0600

    Created the findServicesMatch() function.

---
+++ src/areas.js
@@ -0,0 +1,20 @@
+import { getAreas, getServices, getAreaServices } from "./database.js"
+
+const areas = getAreas()
+const serviceArray = getServices()
+const areaServiceArray = getAreaServices()
+
+const findServicesMatch = (area, areaServiceArray, serviceArray) => {
+    let services = []
+
+    for (const areaService of areaServiceArray) {
+        for (const service of serviceArray) {
+            if (service.id === areaService.serviceId && areaService.areaId === area.id) {
+                services.push(service)
+            }
+        }
+    }
+    return services
+}
+
+
